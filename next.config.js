d5e5847beef7b1f326595d1e80bcf55cce246493
Fabin Paul Francis <francisfabin860@gmail.com>
2026-05-13 16:52:48 +0530

    Deploy: Triggering fresh build with platform-independent dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+// Triggering Vercel redeploy with latest fixes
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ['10.11.1.100', 'localhost:3000'],
